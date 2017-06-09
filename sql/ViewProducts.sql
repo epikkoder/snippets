1c92635182ba474966d496ad8737a6100a465a43
epikkoder <ronaldsahagun87@gmail.com>
2017-06-09 15:55:05 -0700

    Create ViewProducts.sql

--- sql/ViewProducts.sql
+++ sql/ViewProducts.sql
@@ -1,12 +1,12 @@
 WITH coreimages AS (
 SELECT productid
      , [Main Product Image]
-	 , [Main Product Video]
-	 , [Secondary Product Image]
+     , [Main Product Video]
+     , [Secondary Product Image]
 FROM (
 	SELECT pim.productid
 	     , i.imageurl
-		 , mt.NAME AS mediatype
+	     , mt.NAME AS mediatype
     FROM dbo.productimages pim
 	INNER JOIN dbo.images i ON pim.imageid = i.id
 	INNER JOIN dbo.mediatypes mt ON mt.id = i.mediatypeid) PImages
@@ -14,16 +14,16 @@ FROM (
 )
 
 SELECT p.Id
-	    , p.title
-		, p.description
-		, p.baseprice
-		, p.createdby
-		, p.createddate
-		, p.modifiedby
-		, p.modifieddate
-		, p.producttype
-		, ci.[Main Product Image]
-		, ci.[Secondary Product Image]
-		, ci.[Main Product Video]
+     , p.title
+     , p.description
+     , p.baseprice
+     , p.createdby
+     , p.createddate
+     , p.modifiedby
+     , p.modifieddate
+     , p.producttype
+     , ci.[Main Product Image]
+     , ci.[Secondary Product Image]
+     , ci.[Main Product Video]
 FROM coreimages ci
 RIGHT OUTER JOIN dbo. product p ON p.id = ci.productid
